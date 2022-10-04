075977e221cc8ba30bfc1a479b864edd1fb442b4
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-04 20:28:54 -0300

    Requisito 5: concluído

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ let paletaCores = document.getElementById('color-palette');
 // Adiciona à página uma paleta contendo 4 cores distintas, sendo a primeira cor preta.
 
 function createPalette(numberOfColors, arrayOfcolors){
-  for (let index = 0; index < numberOfColors; index += 1) {
-    let colorSelector = document.createElement('li');
-    colorSelector.classList.add('color')
-    colorSelector.style.backgroundColor = arrayOfcolors[index];
-    paletaCores.appendChild(colorSelector);
+    let savedColors = getColorsFromLocalStorage();
+    if(savedColors) {
+        for (let index = 0; index < savedColors.length; index += 1) {
+            let colorSelector = document.createElement('li');
+            colorSelector.classList.add('color')
+            colorSelector.style.backgroundColor = savedColors[index];
+            paletaCores.appendChild(colorSelector);
+        }
+    } else {
+        for (let index = 0; index < numberOfColors; index += 1) {
+            let colorSelector = document.createElement('li');
+            colorSelector.classList.add('color')
+            colorSelector.style.backgroundColor = arrayOfcolors[index];
+            paletaCores.appendChild(colorSelector);
+        }
     }
+    addColorsToLocalStorage();
 }
 
 let arrayOfColors = ['black', 'green', 'red', 'blue'];
 createPalette(arrayOfColors.length, arrayOfColors);
+
 
 // Adiciona um botão para gerar cores aleatórias para a paleta de cores.
 
@@ -27,10 +39,35 @@ function createRandomPaletteButton(){
 
         for (let index = 0; index < itensPaleta.length; index += 1) {
             if(itensPaleta[index].style.backgroundColor !== 'black') {
-                let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-                itensPaleta[index].style.backgroundColor = randomColor;
+                itensPaleta[index].style.backgroundColor = createRandomColor();
             }       
-        }
+        } 
+        addColorsToLocalStorage();
     });
 }
+
+// Função que gera uma cor aleatória
+function createRandomColor(){
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+createRandomPaletteButton();
+
 // Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página.
+
+function addColorsToLocalStorage(){
+    let itensPaleta = document.getElementsByClassName('color');
+    let colorsToLocalStorage = [];
+    for (let index = 0; index < itensPaleta.length; index += 1) {
+        let color = itensPaleta[index].style.backgroundColor;
+        colorsToLocalStorage.push(color);     
+    }
+    localStorage.setItem('colorPalette', JSON.stringify(colorsToLocalStorage));
+} 
+
+function getColorsFromLocalStorage(){
+    return JSON.parse(localStorage.getItem('colorPalette'));
+}
+
+
+// function initialize
