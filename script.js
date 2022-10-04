c97ff8a8d6dd23b463a02ba4b7cf7258a1673ef7
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-04 19:34:53 -0300

    Requisito 4: concluído

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let paletaCores = document.getElementById('color-palette');
-console.log(paletaCores);
+
+// Adiciona à página uma paleta contendo 4 cores distintas, sendo a primeira cor preta.
 
 function createPalette(numberOfColors, arrayOfcolors){
   for (let index = 0; index < numberOfColors; index += 1) {
@@ -10,5 +11,26 @@ function createPalette(numberOfColors, arrayOfcolors){
     }
 }
 
-let arrayOfcolors = ['black', 'green', 'red', 'blue'];
-createPalette(4, arrayOfcolors);
+let arrayOfColors = ['black', 'green', 'red', 'blue'];
+createPalette(arrayOfColors.length, arrayOfColors);
+
+// Adiciona um botão para gerar cores aleatórias para a paleta de cores.
+
+function createRandomPaletteButton(){
+    let randomColorButton = document.createElement('button');
+    randomColorButton.id = 'button-random-color';
+    randomColorButton.innerHTML = 'Cores aleatórias';
+    document.body.appendChild(randomColorButton);
+
+    randomColorButton.addEventListener('click', function(){
+        let itensPaleta = document.getElementsByClassName('color');
+
+        for (let index = 0; index < itensPaleta.length; index += 1) {
+            if(itensPaleta[index].style.backgroundColor !== 'black') {
+                let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+                itensPaleta[index].style.backgroundColor = randomColor;
+            }       
+        }
+    });
+}
+// Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página.
